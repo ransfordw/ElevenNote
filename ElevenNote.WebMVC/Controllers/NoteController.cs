3adb3b2ea3df65c2fc8c38d1ad412bd0e01dd18d
Ransford Walker <ransfordw@gmail.com>
2018-10-15 11:04:00 -0400

    added create and list models and views

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        [Authorize] 
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
