dc71d42acab4cc078b623f668ca9fcb2a1023299
Ransford Walker <ransfordw@gmail.com>
2018-10-16 10:34:54 -0400

    added api methods to controller

--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,64 @@ using System.Web.Http;
 
 namespace ElevenNote.WebApi.Controllers
 {
+    [Authorize]
     public class NoteController : ApiController
     {
+        public IHttpActionResult GetAll()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+
+        private NoteService CreateNoteService()
+        {
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userID);
+            return service;
+        }
     }
 }
